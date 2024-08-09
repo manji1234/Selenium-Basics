b0348ea1bb5dfa20286b29facca32d1e8cd960af
Lenovo <Lenovo@DESKTOP-1580EJV>
2024-08-09 12:27:05 +0530

    committed

---
+++ selenium_maven_project/src/test/java/automation/Base.java
@@ -0,0 +1,39 @@
+package automation;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base 
+{
+ public WebDriver driver;
+ public void initializeBrowser(String browsername)
+ {
+	if(browsername.equals("Chrome"))
+			{
+		      driver=new ChromeDriver();
+			}
+	else if(browsername.equals("Edge"))
+	{
+		driver=new FirefoxDriver();
+	}
+	else
+	{              //constructor      
+		throw new RuntimeException("Invalid Browser Received");
+	}
+	driver.manage().window().maximize();
+ }
+ @BeforeMethod
+ public void setup()
+ {
+	 initializeBrowser("Chrome");   //this.
+ }
+ @AfterMethod
+ public void closeBrowser()
+ {
+	 driver.close();
+ }
+ 
+}
